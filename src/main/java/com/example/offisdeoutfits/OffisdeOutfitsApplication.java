f7427a7005e29dfa843df36828109e57f9412089
isym444 <samir.yep@gmail.com>
2024-04-03 15:08:50 +0100

    first commit

---
+++ src/main/java/com/example/offisdeoutfits/OffisdeOutfitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.offisdeoutfits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffisdeOutfitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OffisdeOutfitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/offisdeoutfits/OffisdeOutfitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.offisdeoutfits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffisdeOutfitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
