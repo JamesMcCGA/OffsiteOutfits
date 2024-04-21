73adec1e036e5f10d582a7eddbfcfea39753960a
isym444 <samir.yep@gmail.com>
2024-04-21 15:36:40 +0100

    configured cors security settings to allow front-end to back-end connection

--- src/main/java/com/example/offsideoutfits/config/SecurityConfig.java
+++ src/main/java/com/example/offsideoutfits/config/SecurityConfig.java
@@ -10,6 +10,13 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -45,8 +52,32 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/applications").hasRole("USER")
                         .anyRequest().authenticated())
                 .httpBasic(withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable());
         return http.build();
+    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Collections.singletonList("http://127.0.0.1:5500")); // or use Arrays.asList() for multiple origins
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:5500"); // Allow frontend server
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*"); // You can list specific methods like "GET", "POST", etc.
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 
